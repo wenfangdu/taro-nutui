5362d45e2d2e3bccbba1aaf3cc67b51e9b1067cd
Wenfang Du <no_reply_mail@163.com>
2023-05-31 22:40:55 +0800

    refactor: prettier codebase

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
-import Components from 'unplugin-vue-components/webpack';
-import NutUIResolver from '@nutui/nutui-taro/dist/resolver';
+import Components from 'unplugin-vue-components/webpack'
+import NutUIResolver from '@nutui/nutui-taro/dist/resolver'
+import { join } from 'node:path'
 
 const config = {
   projectName: 'taro-nutui',
   date: '2023-5-18',
-  designWidth (input) {
+  designWidth(input) {
     if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
       return 375
     }
@@ -14,63 +15,66 @@ const config = {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2,
-    375: 2 / 1
+    375: 2 / 1,
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
   plugins: ['@tarojs/plugin-html'],
-  defineConstants: {
-  },
+  defineConstants: {},
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {},
   },
   framework: 'vue3',
   compiler: {
     type: 'webpack5',
-    prebundle: { enable: false }
-  }, 
-  cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    prebundle: { enable: false },
   },
-  sass:{
-    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+  cache: {
+    enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  sass: {
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`,
   },
   mini: {
     webpackChain(chain) {
-      chain.plugin('unplugin-vue-components').use(Components({
-        resolvers: [NutUIResolver({taro: true})]
-      }))
+      chain.resolve.alias.set('@', join(__dirname, '..', 'src'))
+
+      chain.plugin('unplugin-vue-components').use(
+        Components({
+          resolvers: [NutUIResolver({ taro: true })],
+        }),
+      )
     },
     postcss: {
       pxtransform: {
         enable: true,
         config: {
           // selectorBlackList: ['nut-']
-        }
+        },
       },
       url: {
         enable: true,
         config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
+          limit: 1024, // 设定转换尺寸上限
+        },
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
   },
   h5: {
     webpackChain(chain) {
-      chain.plugin('unplugin-vue-components').use(Components({
-        resolvers: [NutUIResolver({taro: true})]
-      }))
+      chain.plugin('unplugin-vue-components').use(
+        Components({
+          resolvers: [NutUIResolver({ taro: true })],
+        }),
+      )
     },
     publicPath: '/',
     staticDirectory: 'static',
@@ -78,18 +82,17 @@ const config = {
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {},
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  }
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
 }
 
 module.exports = function (merge) {
