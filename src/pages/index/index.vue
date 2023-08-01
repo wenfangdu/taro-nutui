9fdf32156edba34e5e229d0fc482983b46504dff
Wenfang Du <no_reply_mail@163.com>
2023-08-01 16:49:25 +0800

    added repro

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,54 +1,25 @@
 <template>
-  <view class="index">
-    <view>
-      <img src="" alt="" />
-    </view>
-    {{ msg }}
-    <Dongdong />
-    <view class="btn">
-      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
-    </view>
-    <nut-toast :msg="msg2" v-model:visible="show" :type="type" :cover="cover" />
-  </view>
+  <scroll-view
+    style="height: 500px; width: 100%; background-color: red"
+    :scroll-y="true"
+    :refresher-enabled="true"
+    :refresher-threshold="100"
+    :refresher-triggered="state.triggered"
+    @refresherrefresh="onRefresh"
+  ></scroll-view>
 </template>
 
-<script>
-  import { reactive, toRefs } from 'vue'
-  import { Dongdong } from '@nutui/icons-vue-taro'
+<script lang="ts" setup>
+  import { reactive } from 'vue'
 
-  export default {
-    components: {
-      Dongdong,
-    },
-    setup() {
-      const state = reactive({
-        msg: '欢迎使用 NutUI4.0 开发小程序',
-        msg2: '你成功了～',
-        type: 'text',
-        show: false,
-        cover: false,
-      })
+  const state = reactive({
+    triggered: false,
+  })
 
-      const handleClick = (type, msg, cover = false) => {
-        state.show = true
-        state.msg2 = msg
-        state.type = type
-        state.cover = cover
-      }
-
-      return {
-        ...toRefs(state),
-        handleClick,
-      }
-    },
+  const onRefresh = (): void => {
+    state.triggered = true
+    setTimeout(() => {
+      state.triggered = false
+    }, 2000)
   }
 </script>
-
-<style lang="scss">
-  .index {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
-</style>
